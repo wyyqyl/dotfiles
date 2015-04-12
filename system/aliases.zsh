6233b2b50006754b5a15a37b6a3314037244d71d
Stephen <wyyqyl@gmail.com>
2015-04-12 22:25:23 +0800

    add alias for tmux and covenient editing

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,14 @@
+# for ls
 alias ls='ls --color=auto'
 alias ll='ls -ahlF'
 alias la='ls -A'
 alias l='ls -CF'
-alias tmux='tmux -2'
+
+# for tmux
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
 
 alias yyupdate="sudo apt-get update && sudo apt-get dist-upgrade -y"
 alias yyapk="adb install -r "
@@ -11,3 +17,7 @@ alias -s gz='tar xzvf'
 alias -s tgz='tar xzvf'
 alias -s zip='unzip'
 alias -s bz2='tar xjvf'
+
+# convenient for editing configs
+alias ev='vim ~/.vimrc'
+alias ez='vim ~/.zshrc'
