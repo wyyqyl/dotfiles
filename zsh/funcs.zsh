45bb87dca1fcc5e94b0872b3d572175b3e7aa59e
Stephen <wyyqyl@gmail.com>
2015-04-13 21:46:31 +0800

    add logcat function

---
+++ zsh/funcs.zsh
@@ -0,0 +1,26 @@
+logcat () {
+    app_package=$1
+    shift
+
+    if [ -z $app_package ]; then
+        echo "Usage: $(basename $0) [package] ..."
+        return 1
+    fi
+
+    info=$(adb $@ shell ps | awk '$NF~"'"$app_package"'" {print $2, $NF}')
+    if [ -z $info ]; then
+        echo >&2 "Error: app $app_package not running"
+        return 1
+    fi
+
+    echo '----------------------------------------------------------------------'
+    echo $info
+    echo '----------------------------------------------------------------------'
+    pids=$(echo $info | awk '{print $1}')
+    pattern=""
+    while read -r pid; do
+        pattern="$pattern\|([[:space:]]*$pid)"
+    done <<< $pids
+    pattern=$(echo $pattern | cut -c 3-)
+    exec adb $@ logcat | grep -e $pattern --color=never
+}
