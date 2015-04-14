1c4b3f2f3cb5a4a13a1601c1578733c6215c939d
Stephen <wyyqyl@gmail.com>
2015-04-14 15:07:26 +0800

    remove aliases for ls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,12 +1,8 @@
-# for ls
-alias ls='ls --color=auto'
-alias ll='ls -ahlF'
-alias la='ls -A'
-alias l='ls -CF'
-
+# my customized aliases
 alias yyupdate="sudo apt-get update && sudo apt-get dist-upgrade -y"
 alias yyapk="adb install -r "
 
+# for open files directly
 alias -s gz='tar xzvf'
 alias -s tgz='tar xzvf'
 alias -s zip='unzip'
