a51a582b18e41253a2e92f2632a606fbc83c8522
Stephen <wyyqyl@gmail.com>
2015-04-14 15:08:08 +0800

    remove system-specific items

--- system/path.zsh
+++ system/path.zsh
@@ -1,10 +1,19 @@
+# global variable
 PROGRAM_FILES="/data/Programs"
+
+# golang
+export GOROOT="$PROGRAM_FILES/go"
+export GOPATH="/data/workspace/go"
+export PATH="$PATH:$GOPATH/bin"
+
+# android sdk & ndk
+JAVA_HOME="$PROGRAM_FILES/jdk1.8.0_40"
+ANDROID_HOME="/data/Programs/Android/Sdk"
+ANDROID_NDK_HOME="/data/Programs/Android/Ndk"
+export PATH="$PATH:$JAVA_HOME/bin:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME"
+
+# cross compiling
 ARM_NONE="$PROGRAM_FILES/arm-2014.05"
 LINARO_ARM="$PROGRAM_FILES/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabi"
 LINARO_AARCH64="$PROGRAM_FILES/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu"
-
-authenticator=/data/workspace/secclimobile/android/authenticator
-
-export JAVA_HOME="$PROGRAM_FILES/jdk1.8.0_40"
-export PATH="$PATH:$JAVA_HOME/bin:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME:$GOROOT/bin"
 export PATH="$PATH:$ARM_NONE/bin:$LINARO_ARM/bin:$LINARO_AARCH64/bin"
