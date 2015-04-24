854263642be91a43a10b16e603fc762e239d9f7b
Stephen <wyyqyl@gmail.com>
2015-04-24 16:31:05 +0800

    give up vi-mode

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,10 +1,5 @@
 # load functions
 autoload -U zmv
 
-# for vi-mode
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-bindkey '^r' history-incremental-search-backward
-
 # disable auto rename title
 DISABLE_AUTO_TITLE=true
