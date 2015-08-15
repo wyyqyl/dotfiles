8b404e9e24ca488e399cc9eb7bc818cd14d8f1c7
Stephen <wyyqyl@gmail.com>
2015-08-15 18:25:12 +0800

    add alias for apk

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,12 +1,9 @@
-# my customized aliases
-alias yyupdate="sudo apt-get update && sudo apt-get dist-upgrade -y"
-alias yyapk="adb install -r "
-
 # for open files directly
 alias -s gz='tar xzvf'
 alias -s tgz='tar xzvf'
 alias -s zip='unzip'
 alias -s bz2='tar xjvf'
+alias -s apk='adb install -r'
 
 # convenient for editing configs
 alias ev='vim ~/.vimrc'
